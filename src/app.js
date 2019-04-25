d8c24d4c6bb531fe61b4cdd1735bc3ed7bfa0df7
Sirikon <hello@sirikon.me>
2019-04-25 20:17:21 +0200

    It works

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+function jsonGenerator(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.write(JSON.stringify({ message: 'ok!' }));
+  res.end();
+}
+
+function csvGenerator(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  const lines = 75000;
+
+  let c = 0;
+
+  for(var i = 0; i < lines; i++) {
+    setTimeout(() => {
+      res.write(`"${c}";"heyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyheyhey"\n`);
+      c++;
+      if (c === lines) {
+        res.end();
+      }
+    }, 1);
+  }
+}
+
+module.exports = () => {
+  const app = express();
+  
+  app.use(express.static('./src/public'));
+
+  app.get('/json-generator', jsonGenerator)
+  app.get('/csv-generator', csvGenerator)
+  
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+}
